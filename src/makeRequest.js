45ecc1c67ec3df5f3fa3af6defefe3f8f66b9855
markdalgleish <mark.john.dalgleish@gmail.com>
2015-01-09 14:54:55 +1100

    Use xhr2 to reuse XMLHttpRequest code in Node
    The 'browser' property is used in package.json
    so that Browserify doesn't include xhr2 in the
    browser bundle.

--- src/makeRequest.js
+++ src/makeRequest.js
@@ -1,40 +1,16 @@
 var makeRequest = function(method, url, body, callback) {
-  var http = require('http');
-  var parse = require('url').parse;
-
-  var parsedUrl = parse(url);
-  var requestOptions = {
-    hostname: parsedUrl.hostname,
-    port: parsedUrl.port,
-    method: method,
-    path: parsedUrl.path,
-    headers: {
-      'X-Pact-Mock-Service': 'true',
-      'Content-Type': 'application/json'
-    }
+  var XMLHttpRequest = require('xhr2');
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function(event) {
+    callback(null, event.target);
   };
-
-  var request = http.request(requestOptions, function (response) {
-    var responseText = '';
-
-    response.on('data', function (chunk) {
-      responseText += chunk.toString();
-    });
-
-    response.on('error', function (err) {
-      callback(new Error('Error calling ' + url + ' - ' + err.message));
-    });
-
-    response.on('end', function () {
-      callback(null, {responseText: responseText, status: response.statusCode});
-    });
-  });
-
-  request.on('error', function (err) {
-    callback(new Error('Error calling ' + url + ' - ' + err.message));
-  });
-
-  request.end(body || '');
+  xhr.onerror = function() {
+    callback(new Error('Error calling ' + url));
+  };
+  xhr.open(method, url, true);
+  xhr.setRequestHeader('X-Pact-Mock-Service', 'true');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(body);
 };
 
 module.exports = makeRequest;
